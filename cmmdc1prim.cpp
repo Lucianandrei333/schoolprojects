d1a0f5a94cb80f1955f0e5e89899d3bf4a734ea8
Lucianandrei333 <44975419+Lucianandrei333@users.noreply.github.com>
2018-11-12 07:19:47 -0800

    Add files via upload

---
+++ cmmdc1prim.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int a;
+int b;
+int main ()
+{ cout<<"a="; cin>>a;
+cout<<"b="; cin>>b;
+ if (a=b){cout<<"a";}
+  else {
+    do{
+        if(a>b){ a==a-b;}
+        else { b==b-a;}
+
+        } while (a!=b);
+        cout<<"a";
+
+}
+
+
+
+
+
+
+
+
+
+
+}
